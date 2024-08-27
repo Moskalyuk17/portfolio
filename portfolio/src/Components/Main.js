82085b574e7a81c8155cd98ffad89e8771fc596a
Moskalyuk17 <mamaliuk06@mail.ru>
2024-08-27 12:57:48 +0300

    first

--- portfolio/src/Components/Main.js
+++ portfolio/src/Components/Main.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom"
+
 import logo from '../assect/people.png';
 import Aside from './Aside';
 
-class Main extends React.Component {
-    render () {
-        return (
-            <div className='main'>
-                <div className='main_container'>
-                    <div className='circle'></div>
-                    <figure>
-                        <img className='logo' src={logo} />
-                    </figure>
-                    <Aside />
-                </div>
+export function Main() {
+    return (
+        <div className='main'>
+            <div className='main_container'>
+                <div className='circle'></div>
+                <figure>
+                    <img className='logo' alt='man' src={logo} />
+                </figure>
+                <Routes>
+                    <Route path='/' element={<Main />} />
+                    <Route path='/aside' element={<Aside />} />
+                </Routes>
             </div>
-          );
-    }
+        </div>
+    );
 }
+
 
 export default Main;
