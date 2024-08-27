1d79fb5c12183f2d07655fc2d567c23312045c31
Moskalyuk17 <mamaliuk06@mail.ru>
2024-08-27 13:07:48 +0300

    first

--- portfolio/src/Components/Aside.js
+++ portfolio/src/Components/Aside.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { Route, Routes, Link } from "react-router-dom"
-import Home from './Home';
 import About from './About';
-import Contacts from './Contacts';
-import Project from './Project';
-import App from '../App';
 
-export function Aside() {
-    return (
-      <>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/books">Books</Link></li>
-            <li><Link to="/books">Books</Link></li>
-            <li><Link to="/books">Books</Link></li>
-          </ul>
-        </nav>
-  
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<About />} />
-          <Route path="/books" element={<Project />} />
-          <Route path="/books" element={<Contacts />} />
-        </Routes>
-      </>
-    )
-  }
- 
+class Aside extends React.Component {
+    render () {
+        return (
+            <div className='aside'>
+                <div className='aside_info'>
+                    <a href='./About' className='about'>ABOUT</a>
+                    <a href='#' className='project'>PROJECT</a>
+                    <a href='#' className='contacts'>CONTACTS</a>
+                </div>
+            </div>
+          );
+    }
+}
 
 export default Aside;
--- portfolio/src/Components/Main.js
+++ portfolio/src/Components/Main.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom"
-
 import logo from '../assect/people.png';
 import Aside from './Aside';
 
-export function Main() {
-    return (
-        <div className='main'>
-            <div className='main_container'>
-                <div className='circle'></div>
-                <figure>
-                    <img className='logo' alt='man' src={logo} />
-                </figure>
-                <Routes>
-                    <Route path='/' element={<Main />} />
-                    <Route path='/aside' element={<Aside />} />
-                </Routes>
+class Main extends React.Component {
+    render () {
+        return (
+            <div className='main'>
+                <div className='main_container'>
+                    <div className='circle'></div>
+                    <figure>
+                        <img className='logo' alt='man' src={logo} />
+                    </figure>
+                    <Aside />
+                </div>
             </div>
-        </div>
-    );
+          );
+    }
 }
-
 
 export default Main;
